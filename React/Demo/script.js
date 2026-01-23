305994cdd18c8e4959fcaa34c30c13d44dfc328c
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2026-01-23 13:14:59 +0530

    Added:React Basics

--- React/Demo/script.js
+++ React/Demo/script.js
@@ -1,7 +1,14 @@
-let ele = React.createElement("h1", null, "Hello, From React!");
+let ele1 = React.createElement("h1", { id: "h1" }, "Hello, From React!");
+let ele2 = React.createElement("h2", { className: "h2" }, "Hello, From React2!");
 
-let container = document.querySelector(".container");
+// {class:"h2"}/null - attributes
 
-let root = ReactDOM.createRoot(container);
+// rendering multiple elements
+let div = React.createElement("div", null, [ele1, ele2]);
 
-root.render(ele);
+// let container = document.querySelector(".container");
+
+// let root = ReactDOM.createRoot(container);
+let root = ReactDOM.createRoot(document.querySelector(".container"));
+
+root.render(div);
