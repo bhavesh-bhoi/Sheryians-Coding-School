c06fab45120676b82236006acd3ba68357bad4ae
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2026-01-25 19:49:27 +0530

    Added:Like Functionality

--- JavaScript/Practice/Reels Project/script.js
+++ JavaScript/Practice/Reels Project/script.js
@@ -63,11 +63,12 @@ const reels = [
 
 let reelsContainer = document.querySelector(".reels-container");
 
-let allReelsHTML = "";
-isFollowing = false;
+function addReel() {
+  let allReelsHTML = "";
+  isFollowing = false;
 
-reels.forEach(function (reel) {
-  allReelsHTML += `
+  reels.forEach(function (reel, index) {
+    allReelsHTML += `
     <div class="reels">
       <video autoplay loop muted src="${reel.video}"></video>
 
@@ -84,7 +85,7 @@ reels.forEach(function (reel) {
       </div>
 
       <div class="right">
-        <div class="like ${reel.isLiked ? "active" : ""}">
+        <div id=${index} class="like ${reel.isLiked ? "active" : ""}">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round"
               d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
@@ -117,13 +118,21 @@ reels.forEach(function (reel) {
       </div>
     </div>
   `;
-});
-
-reelsContainer.innerHTML = allReelsHTML;
-
-document.querySelectorAll(".follow-btn").forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const isFollowing = btn.classList.toggle("following");
-    btn.textContent = isFollowing ? "Following" : "Follow";
   });
-});
+
+  reelsContainer.innerHTML = allReelsHTML;
+}
+
+addReel(reels);
+
+reelsContainer.addEventListener("click", function(dets){
+  console.log(dets);
+  
+})
+
+// document.querySelectorAll(".follow-btn").forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     const isFollowing = btn.classList.toggle("following");
+//     btn.textContent = isFollowing ? "Following" : "Follow";
+//   });
+// });
