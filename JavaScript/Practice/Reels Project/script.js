8a2f2be83b14beafa936c334b0ccee06a604ddaf
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2026-01-26 15:59:11 +0530

    Added:Like & Follow Functionality

--- JavaScript/Practice/Reels Project/script.js
+++ JavaScript/Practice/Reels Project/script.js
@@ -76,7 +76,7 @@ function addReel() {
         <div class="profile-info">
           <img src="${reel.profileImage}" alt="" />
           <h2>${reel.username}</h2>
-          <h4 class="follow-btn">${reel.isFollowing ? "Following" : "Follow"}</h4>
+          <h4 id=${index} class="follow-btn">${reel.isFollowing ? "Following" : "Follow"}</h4>
         </div>
 
         <div class="description">
@@ -85,13 +85,13 @@ function addReel() {
       </div>
 
       <div class="right">
-        <div id=${index} class="like ${reel.isLiked ? "active" : ""}">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round"
-              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-          </svg>
-          <p>${reel.likes}</p>
-        </div>
+        <div id="${index}" class="like">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="${reel.isLiked ? "red" : "none"}" viewBox="0 0 24 24" stroke-width="1.5" stroke="${reel.isLiked ? "red" : "currentColor"}">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+            </svg>
+            <p>${reel.likes}</p>
+          </div>
 
         <div class="comment">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -125,14 +125,24 @@ function addReel() {
 
 addReel(reels);
 
-reelsContainer.addEventListener("click", function(dets){
-  console.log(dets);
-  
-})
+reelsContainer.addEventListener("click", function (dets) {
+  if (dets.target.className == "like") {
+    if (!reels[dets.target.id].isLiked) {
+      reels[dets.target.id].isLiked = true;
+      reels[dets.target.id].likes++;
+    } else {
+      reels[dets.target.id].isLiked = false;
+      reels[dets.target.id].likes--;
+    }
+    addReel(reels);
+  }
 
-// document.querySelectorAll(".follow-btn").forEach((btn) => {
-//   btn.addEventListener("click", () => {
-//     const isFollowing = btn.classList.toggle("following");
-//     btn.textContent = isFollowing ? "Following" : "Follow";
-//   });
-// });
+  if (dets.target.className == "follow-btn") {
+    if (!reels[dets.target.id].isFollowing) {
+      reels[dets.target.id].isFollowing = true;
+    } else {
+      reels[dets.target.id].isFollowing = false;
+    }
+    addReel(reels);
+  }
+});
