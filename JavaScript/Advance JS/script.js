3ead3f0a177636e7d1c1a00f1d2ee3aba583136c
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2026-02-07 10:44:11 +0530

    Added:Understood Classes and Classes With Constructor

--- JavaScript/Advance JS/script.js
+++ JavaScript/Advance JS/script.js
@@ -9,6 +9,7 @@ let user = {
 console.log(user);
 
 // Classes
+// Classes returns new objects everytime
 
 class SmartPhone {
   constructor() {
@@ -19,7 +20,7 @@ class SmartPhone {
   }
 }
 
-let Phone1 = new SmartPhone()
+let Phone1 = new SmartPhone();
 
 // Class with Constructor
 class SmartPhone1 {
@@ -31,4 +32,32 @@ class SmartPhone1 {
   }
 }
 
-let Phone2 = new SmartPhone1("Samsung", "S", "S25", 115000)
+let Phone2 = new SmartPhone1("Samsung", "S", "S25", 115000);
+
+// Book Class Example
+// Value of this is decided when it is being called
+class Book {
+  constructor(bName, bAuthor, bPrice, bColor) {
+    this.bookName = bName;
+    this.bookAuthor = bAuthor;
+    this.bookPrice = bPrice;
+    this.color = bColor;
+  }
+
+  nextPage() {
+    console.log("Page Changed To Next!");
+  }
+  pauseReadingBook() {
+    console.log("Book Paused!");
+  }
+  previousPage() {
+    console.log("Page Changed To Previous!");
+  }
+}
+
+let book = new Book(
+  "The Power Of Your SubConcious Mind",
+  "Dr. Joseph Murphy",
+  1.5 + "$",
+  "OffWhite",
+);
