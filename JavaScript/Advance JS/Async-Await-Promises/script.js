f3925ca74ba74bbb884eac2d68fb5ecdd537a9a9
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2026-02-09 14:48:06 +0530

    Added:Async-Await Example

--- JavaScript/Advance JS/Async-Await-Promises/script.js
+++ JavaScript/Advance JS/Async-Await-Promises/script.js
@@ -22,3 +22,25 @@ let data = fetch("https://randomuser.me/api/")
     console.log(realData.results[0].name);
   })
   .catch((err) => console.log(err));
+
+// Async-Await
+
+function getNum() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let num = Math.floor(Math.random() * 10);
+      if (num > 5) {
+        resolve(true);
+      } else {
+        reject(false);
+      }
+    }, 2000);
+  });
+}
+
+async function abcd() {
+  let newNum = await getNum();
+  console.log(newNum);
+}
+
+abcd();
