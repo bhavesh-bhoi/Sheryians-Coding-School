134e884598cfc9d1a88268f5a0d176e0607416f2
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2026-02-10 22:15:09 +0530

    Added:Throttling Example

--- JavaScript/Advance JS/Throttling-Debouncing/script.js
+++ JavaScript/Advance JS/Throttling-Debouncing/script.js
@@ -1,3 +1,5 @@
+// Debouncing
+
 function debounce(fn, delay) {
   let timer;
   return function () {
@@ -11,4 +13,28 @@ let search = document.querySelector("#search").addEventListener(
   debounce(function () {
     console.log("Started!!!");
   }, 400),
+);
+
+// Throttling
+
+function throttle(fn, delay) {
+  let last = 0;
+
+  return function () {
+    const now = Date.now();
+    if (now - last >= delay) {
+      last = now;
+      fn();
+    }
+  };
+}
+
+window.addEventListener(
+  "mousemove",
+  function () {
+    throttle(function () {
+      console.log("moving");
+    });
+  },
+  2000,
 );
