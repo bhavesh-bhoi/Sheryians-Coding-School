bbed769eb5e71856b1d9469f39a1299a22aece96
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2026-03-08 11:59:25 +0530

    Added:Context API Task

--- React/class-15-task/src/App.jsx
+++ React/class-15-task/src/App.jsx
@@ -1,18 +1,47 @@
-import axios from 'axios'
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  
+  const [ProductData, setProductData] = useState([]);
+
   const getData = async () => {
-    const response = await axios.get('https://fakestoreapi.com/products')
-    console.log(response.data);
-    
-  }
+    const response = await axios.get("https://fakestoreapi.com/products");
+    setProductData(response.data);
+  };
+
+  // const getRandomData = async (id) => {
+  //   const response = await axios.get(`https://fakestoreapi.com/products/${id}`);
+  //   console.log(response.data);
+  // };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
-    <div>
-      <button onClick={getData}>GET all products</button>
+    <div className="allProducts">
+      {/* <button onClick={getData}>GET All Products</button> */}
+
+      {ProductData.map((product) => {
+        return (
+          <div key={product.id} className="productCard">
+            <h2>{product.title}</h2>
+            <p>{product.description}</p>
+            <img src={product.image} alt={product.title} width="100" />
+          </div>
+        );
+      })}
+      {/* Get Random Product */}
+      {/* <button
+        onClick={() => {
+          const randomId = Math.floor(Math.random() * 20 + 1);
+          getRandomData(randomId);
+        }}
+      >
+        GET Random Product
+      </button> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
